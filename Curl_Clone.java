96048c8dcf2c21182796046bff2acc9c5151c44a
中田愛刀 <Aito-Nakata@users.noreply.github.com>
2021-05-17 11:00:27 +0900

    コマンドライン引数を取得するプログラム作成

---
+++ Curl_Clone.java
@@ -0,0 +1,23 @@
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+
+class Curl_Clone{
+
+}
+
+class Main{
+    public static void main(String args[]){
+      int i;
+        
+      if(args.length == 0){
+
+      }
+      else if(args.length != 0){
+        for(i = 0;  i < args.length; i++){
+          if(args[i].equals("-o")){
+            
+          } 
+        }
+      }
+    }
+  }
